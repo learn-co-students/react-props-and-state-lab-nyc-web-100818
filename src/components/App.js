80e1eec2d576fce26c0198ac2757bce3936fa20f
sachafrosell <sachafrosell@gmail.com>
2018-12-12 11:45:21 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,53 @@ import PetBrowser from './PetBrowser'
 class App extends React.Component {
   constructor() {
     super()
-
     this.state = {
       pets: [],
       filters: {
         type: 'all'
       }
     }
+  }
+
+  filterChange = (val) => {
+    this.setState({
+      filters: {
+        type: val
+      }
+    })
+  }
+
+  fetchPets = () => {
+    let baseURL = '/api/pets'
+    if (this.state.filters.type === 'all') {
+      fetch(baseURL)
+        .then(response => response.json())
+        .then(pets => (
+          this.setState({
+            pets: pets
+          })
+        ))
+    } else {
+      fetch(`${baseURL}?type=${this.state.filters.type}`)
+        .then(response => response.json())
+        .then(pets => (
+          this.setState({
+            pets: pets
+          })
+        ))
+    }
+  }
+
+  onAdoptPet = (id) => {
+    this.setState(prevState => ({
+      pets:
+      (prevState.pets.map(pet => {
+        if (pet.id === id) {
+          pet = {...pet, isAdopted: true}
+        }
+        return pet
+      }))
+    }))
   }
 
   render() {
@@ -24,10 +64,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.filterChange} onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.onAdoptPet} isAdopted={this.isAdopted} pets={this.state.pets}/>
             </div>
           </div>
         </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,52 @@
 import React from 'react'
 
 class Pet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAdopted: this.props.isAdopted
+    }
+  }
+
+  handleClick = () => {
+    this.props.onAdoptPet(this.props.pet.id)
+  }
+
+  adoptedLogic = () => {
+    // console.log(this.props.pet.isAdopted);
+    if (this.props.pet.isAdopted === true) {
+      return <button className="ui disabled button">Already adopted</button>
+    } else {
+      return <button className="ui primary button" onClick={this.handleClick}>Adopt pet</button>
+    }
+  }
+
+  genderLogic = () => {
+    if (this.props.pet.gender === "male") {
+      return "♂"
+    } else {
+      return "♀"
+    }
+  }
+
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {this.genderLogic()}
+            {this.props.pet.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {this.adoptedLogic()}
         </div>
       </div>
     )
