708141eb41e642fe268c33a644be68522f51e095
Eric Laitman <ericlaitman@Erics-MacBook-Pro-2.local>
2019-01-08 19:08:59 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,35 @@ class App extends React.Component {
     }
   }
 
+    onAdoptPet = petId => {
+      const pets = this.state.pets.map(p => {
+        return p.id === petId ? { ...p, isAdopted: true } : p;
+      });
+      this.setState({ pets });
+    };
+
+
+  onChangeType = (props) => {
+    this.setState({filters:{type:props}})
+  }
+
+  onFindPetsClick = () => {
+    if (this.state.filters.type === 'all'){
+        fetch('/api/pets')
+        .then(r=>r.json())
+        .then(r=>{
+          this.setState({pets: r})
+        })
+    }else{
+      fetch(`/api/pets?type=${this.state.filters.type}`)
+      .then(r=>r.json())
+      .then(r=>{
+        this.setState({pets: r})
+      })
+    }
+  }
+
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +53,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.onAdoptPet} petData={this.state.pets}/>
             </div>
           </div>
         </div>
@@ -37,3 +66,108 @@ class App extends React.Component {
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+//
+// import Filters from './Filters'
+// import PetBrowser from './PetBrowser'
+//
+// export default class App extends React.Component {
+//
+//   state = {
+//       pets: [],
+//       filters: {
+//         type: 'all'
+//       }
+//     }
+//
+//   onChangeType = changedType => {
+//     this.setState({filters: {type: changedType}})//, () =>{console.log(this.state.filters.type)})
+//   }
+//
+//   onFindPetsClick = () => {
+//     // console.log("clicked", this.state.filters.type);
+//     if(this.state.filters.type === "all"){
+//       fetch('/api/pets', {method: 'GET'})
+//       .then(res=> res.json())
+//       .then(data=> {
+//         // console.log("Inside fetch all:",data);
+//         this.setState({pets: data})
+//         // console.log("New set state after select all:", this.state.pets);
+//       })
+//     } else {
+//       fetch(`/api/pets?type=${this.state.filters.type}`, {method: 'GET'})
+//       .then(res=> res.json())
+//       .then(data=> {
+//         // console.log("Inside fetch type:",data);
+//         this.setState({pets: data})
+//         // console.log("New set state after select type:", this.state.pets);
+//       })
+//     }
+//   }
+//
+//   // test = () => {
+//   //   console.log("beefysteak")
+//   // }
+//
+//   render() {
+//     return (
+//       <div className="ui container">
+//         <header>
+//           <h1 className="ui dividing header">React Animal Shelter</h1>
+//         </header>
+//         <div className="ui container">
+//           <div className="ui grid">
+//             <div className="four wide column">
+//               <Filters
+//                onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
+//             </div>
+//             <div className="twelve wide column">
+//               <PetBrowser petsArr={this.state.pets}/>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+// //
+// we start with the render method in the app class. we render content. then we get to the filters class. we pass in the method onChangeType. we want to use that method to set the state type. when will we use that method? when there is a new drop down chosen. so we have to listen for a changeevent on the drop down. when the changeevent is activated, we then assign the value chosen to the state. then we need to listen for a submit action. when the submit happens, we want to fetch data and render it to the page. we create a fetch method on the app class and we create the submit listener in the button tag. when the button is clicked, we call the fetch function. in fetch, we make the pets key = all the fetched data. then we call on the petbrowser and pass in the pets array.
+// we then map over the the pet array and pass in each of the values into the pet class. then we render the this.props.age etc. out in html format.
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,9 +3,81 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
-  }
-}
+        return (
+          <div className="ui cards">
+            {this.props.petData.map(pet =>{
+              return <Pet onAdoptPet={this.props.onAdoptPet} key={pet.id} age={pet.age} gender={pet.gender} id={pet.id} adopted={pet.isAdopted} name={pet.name} type={pet.type} weight={pet.weight} />})}
+          </div>
+        )
+      }
+    }
+
 
 export default PetBrowser
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {Fragment} from 'react'
+//
+// import Pet from './Pet'
+//
+// export default class PetBrowser extends React.Component {
+// // constructor(props){
+// //   super(props)
+// // }
+// // age: 3
+// // gender: "male"
+// // id: "2c902312-dfa3-446f-8b4b-5e115170d807"
+// // isAdopted: false
+// // name: "Teddy"
+// // type: "cat"
+// // weight: 1
+//
+//   render() {
+//     // console.log(this.props.petsArr);
+//     return (
+//       <Fragment>
+//         <div className="ui cards">
+//           {this.props.petsArr.map(pet =>{ return <Pet name={pet.name} key={pet.id} gender={pet.gender} isAdopted={pet.isAdopted} age={pet.age} type={pet.type} weight={pet.weight}/>})}
+//         </div>
+//       </Fragment>
+//     )
+//   }
+// }
